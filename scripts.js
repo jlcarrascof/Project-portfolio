a93d209f5b61dea624eeddd066f9914583981a67
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-04-13 18:54:33 -0400

    Fix some linters issues and test eslintrc.json

--- scripts.js
+++ scripts.js
@@ -1,18 +1,5 @@
-/*
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "./assets/images/breadcrumb.png";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "./assets/images/close.png";
-    }
-}
-*/
-
-const myOpen = document.querySelector('.burger');
-const nav = document.querySelector('.header-menu');
+const myOpen = document.querySelector('.hamburger');
+const nav = document.querySelector('.main-menu');
 const myClose = document.querySelector('.close');
 const myLinks = document.querySelector('.links');
 
