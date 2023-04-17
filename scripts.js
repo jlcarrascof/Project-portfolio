66cd36286853ca330df92a5831bdd3a7058ad4f4
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-04-17 17:29:18 -0400

    Try to test a new way to do the same job

--- scripts.js
+++ scripts.js
@@ -12,6 +12,24 @@ myClose.onclick = () => {
   myOpen.classList.remove('display');
 };
 myLinks.onclick = () => {
-  this.parentElement.classList.remove('open');
+  nav.classList.remove('open');
   myOpen.classList.remove('display');
 };
+
+
+/*
+let burguer_menu = document.getElementById('burguer_menu');
+let	slideMenu = document.getElementById('slidemenu');
+let	elem_menu = document.querySelectorAll('#slidemenu a');
+
+function toggleMenu() {
+	slideMenu.classList.toggle("active");
+}
+
+burguer_menu.addEventListener('click', toggleMenu); 
+
+for (let i = 0; i < elem_menu.length; i++) {
+	elem_menu[i].addEventListener('click', toggleMenu);
+}
+*/
+
