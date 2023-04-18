0eb7f8f868b6c5d9b980d7b4bed2eea6fc7cec4c
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-04-18 12:07:10 -0400

    Fix the X in hamburger menu

--- scripts.js
+++ scripts.js
@@ -1,50 +1,16 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const navBar = document.querySelector(".nav-bar");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  navBar.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",
   () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+    navBar.classList.remove("active");
 }))
-
-/*
-const myOpen = document.querySelector('.hamburger');
-const nav = document.querySelector('.main-menu');
-const myClose = document.querySelector('.close');
-const myLinks = document.querySelector('.links');
-
-myOpen.onclick = () => {
-  nav.classList.add('open');
-  myOpen.classList.add('display');
-};
-myClose.onclick = () => {
-  nav.classList.remove('open');
-  myOpen.classList.remove('display');
-};
-myLinks.onclick = () => {
-  nav.classList.remove('open');
-  myOpen.classList.remove('display');
-};
-*/
-
-/*
-let burguer_menu = document.getElementById('burguer_menu');
-let	slideMenu = document.getElementById('slidemenu');
-let	elem_menu = document.querySelectorAll('#slidemenu a');
-
-function toggleMenu() {
-	slideMenu.classList.toggle("active");
-}
-
-burguer_menu.addEventListener('click', toggleMenu); 
-
-for (let i = 0; i < elem_menu.length; i++) {
-	elem_menu[i].addEventListener('click', toggleMenu);
-}
-*/
-
