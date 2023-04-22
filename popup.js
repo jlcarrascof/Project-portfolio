3487d012fcd8cf30cd10c979cae882d4e76cf56f
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-04-22 10:36:23 -0400

    Fix linters errors

--- popup.js
+++ popup.js
@@ -2,83 +2,69 @@
 /* each project has: name, ShortDescrip, featured image, technologies,
    link to live version, link to source */
 
-   const arrCardsInfo = [
-    {
-      id: 'card-0',
-      name: 'Tonic',
-      name2: 'Canopy',
-      ShortDescrip: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-      LongDescrip: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-      cardImage: './assets/images/image1.svg',
-      alternateTextImage: 'header-main-project image.',
-      technologies: ['HTML', 'CSS', 'Javascript'],
-      liveVersion: 'https://jlcarrascof.github.io/card-portfolio/',
-      source: 'https://github.com/jlcarrascof/card-portfolio/'
-    },
-    {
-      id: 'card-1',
-      name: 'Multi-Post Stories',
-      name2: 'Canopy',
-      ShortDescrip: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-      LongDescrip: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-      cardImage: './assets/images/image2.svg',
-      alternateTextImage: 'secondary project ShortDescrip image.',
-      technologies: ['CSS', 'Boostrap', 'Ruby'],
-      liveVersion: 'https://jlcarrascof.github.io/card-portfolio/',
-      source: 'https://github.com/jlcarrascof/card-portfolio/'
-    },
-    {
-      id: 'card-2',
-      name: 'Profesional Art 2',
-      name2: 'Printing Data',
-      ShortDescrip: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-      LongDescrip: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-      cardImage: './assets/images/image3.svg',
-      alternateTextImage: 'secondary project ShortDescrip image.',
-      technologies: ['CSS', 'Boostrap', 'Ruby'],
-      liveVersion: 'https://jlcarrascof.github.io/card-portfolio/',
-      source: 'https://github.com/jlcarrascof/card-portfolio/'
-    },
-    {
-      id: 'card-3',
-      name: 'Profesional Art 3',
-      name2: 'Printing Data',
-      ShortDescrip: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-      LongDescrip: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-      cardImage: './assets/images/image4.svg',
-      alternateTextImage: 'secondary project ShortDescrip image.',
-      technologies: ['CSS', 'Boostrap', 'Ruby'],
-      liveVersion: 'https://jlcarrascof.github.io/card-portfolio/',
-      source: 'https://github.com/jlcarrascof/card-portfolio/'
-    },
-    /*
-    {
-      id: 'card-4',
-      name: 'Profesional Art 4',
-      name2: 'Printing Data',
-      ShortDescrip: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-      LongDescrip: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-      cardImage: './assets/images/image2.svg',
-      alternateTextImage: 'secondary project ShortDescrip image.',
-      technologies: ['CSS', 'Boostrap', 'Ruby'],
-      liveVersion: 'https://jlcarrascof.github.io/card-portfolio/',
-      source: 'https://github.com/jlcarrascof/card-portfolio/'
-    }
-    */
-  ];
-  
-  const gridWorks = document.getElementById('grid_works');
-  let winPopup = null;
-  let div = null;
-  let loadBody = null;
-  let card = null;
-  
-  for (let i = 0; i < arrCardsInfo.length; i += 1) {
-    card = arrCardsInfo[i];
-  
-    div = document.createElement('div');
-    div.id = card.id;
-    div.innerHTML = `
+const arrCardsInfo = [
+  {
+    id: 'card-0',
+    name: 'Tonic',
+    name2: 'Canopy',
+    ShortDescrip: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    LongDescrip: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    cardImage: './assets/images/image1.svg',
+    alternateTextImage: 'header-main-project image.',
+    technologies: ['HTML', 'CSS', 'Javascript'],
+    liveVersion: 'https://jlcarrascof.github.io/card-portfolio/',
+    source: 'https://github.com/jlcarrascof/card-portfolio/',
+  },
+  {
+    id: 'card-1',
+    name: 'Multi-Post Stories',
+    name2: 'Canopy',
+    ShortDescrip: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
+    LongDescrip: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    cardImage: './assets/images/image2.svg',
+    alternateTextImage: 'secondary project ShortDescrip image.',
+    technologies: ['CSS', 'Boostrap', 'Ruby'],
+    liveVersion: 'https://jlcarrascof.github.io/card-portfolio/',
+    source: 'https://github.com/jlcarrascof/card-portfolio/',
+  },
+  {
+    id: 'card-2',
+    name: 'Profesional Art 2',
+    name2: 'Printing Data',
+    ShortDescrip: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
+    LongDescrip: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    cardImage: './assets/images/image3.svg',
+    alternateTextImage: 'secondary project ShortDescrip image.',
+    technologies: ['CSS', 'Boostrap', 'Ruby'],
+    liveVersion: 'https://jlcarrascof.github.io/card-portfolio/',
+    source: 'https://github.com/jlcarrascof/card-portfolio/',
+  },
+  {
+    id: 'card-3',
+    name: 'Profesional Art 3',
+    name2: 'Printing Data',
+    ShortDescrip: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
+    LongDescrip: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    cardImage: './assets/images/image4.svg',
+    alternateTextImage: 'secondary project ShortDescrip image.',
+    technologies: ['CSS', 'Boostrap', 'Ruby'],
+    liveVersion: 'https://jlcarrascof.github.io/card-portfolio/',
+    source: 'https://github.com/jlcarrascof/card-portfolio/',
+  },
+];
+
+const gridWorks = document.getElementById('grid_works');
+let winPopup = null;
+let div = null;
+let loadBody = null;
+let card = null;
+
+for (let i = 0; i < arrCardsInfo.length; i += 1) {
+  card = arrCardsInfo[i];
+
+  div = document.createElement('div');
+  div.id = card.id;
+  div.innerHTML = `
     <div class="cards card-1">
                 <div>
                     <img src="${card.cardImage}" alt="${card.alternateTextImage}" class="screenshot">
@@ -110,14 +96,14 @@
                         <ul>
                         ${card.technologies.map((tech) => `
                         <li class="li_btn_2">${tech}</li>`).join(' ')
-                        }
+}
                         </ul>
                     </div>
                     <div class="card-skills desktop">
                         <ul>
                         ${card.technologies.map((tech) => `
                         <li class="li_btn_2">${tech}</li>`).join(' ')
-                        }
+}
                         </ul>
                     </div>
                     <div>
@@ -127,36 +113,34 @@
             </div>
             `;
 
-    gridWorks.appendChild(div);
-  }
+  gridWorks.appendChild(div);
+}
 
 // functions realted with popup window
-  
-  function closeWinPopup() {
-    loadBody.removeChild(winPopup);
-  
-    /* re-activate the scrollbar for the body */
-    loadBody.style.overflow = 'auto'; 
-  }
-  
-  function openLink(url) {
-    window.open(url);
-    closeWinPopup();
-  }
-  
+
+function closeWinPopup() {
+  loadBody.removeChild(winPopup);
+
+  /* re-activate the scrollbar for the body */
+  loadBody.style.overflow = 'auto';
+}
+
+function openLink(url) {
+  window.open(url);
+  closeWinPopup();
+}
 
 // create popup window
-  
+
 function openPopup(card) {
   loadBody = document.getElementById('loadBody');
 
   /* Hide scrollbar for the body */
-  loadBody.style.overflow = 'hidden'; 
+  loadBody.style.overflow = 'hidden';
 
   loadBody.appendChild(document.createElement('div')).setAttribute('id', 'winPopup');
   winPopup = document.getElementById('winPopup');
-  winPopup.classList = "winPopup";
-  console.log("entre a crear popup");
+  winPopup.classList = 'winPopup';
 
   /* *********************************** */
   div = document.createElement('div');
@@ -198,14 +182,14 @@ function openPopup(card) {
                             <ul>
                             ${card.technologies.map((tech) => `
                             <li class="li_btn_2">${tech}</li>`).join(' ')
-                            }
+}
                             </ul>
                         </div>
                         <div class="card-skills desktop">
                             <ul>
                               ${card.technologies.map((tech) => `
                               <li class="li_btn_2">${tech}</li>`).join(' ')
-                              }
+}
                             </ul>
                         </div>
                         <div class="btnlinks">
@@ -231,8 +215,8 @@ function openPopup(card) {
   const btnSrc = document.getElementById('btnGit');
 
   btnClose.addEventListener('click', closeWinPopup);
-  btnLive.addEventListener('click', () => {openLink(card.liveVersion); });
-  btnSrc.addEventListener('click', () => {openLink(card.source); });
+  btnLive.addEventListener('click', () => { openLink(card.liveVersion); });
+  btnSrc.addEventListener('click', () => { openLink(card.source); });
 }
 
 /* ********************************************** */
